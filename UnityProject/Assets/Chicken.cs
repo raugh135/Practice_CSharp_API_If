b6635bee039b59600b0d653617604bed8cb435c7
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-17 12:04:05 +0800

    Chicken

---
+++ UnityProject/Assets/Chicken.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour {
+
+    public Animator m_animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            m_animator.SetTrigger("吃飯觸發器");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            m_animator.SetBool("跑步開關",true);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            m_animator.SetBool("跑步開關", false);
+        }
+    }
+}
